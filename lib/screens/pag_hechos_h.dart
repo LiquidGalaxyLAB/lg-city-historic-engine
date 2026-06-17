fdca477aa3c0641364e197e4bcf20cbd64758724
Yasmina-R-S <yasiramadan@gmail.com>
2026-06-17 12:40:32 +0200

    17/06/2026
    informacion

--- lib/screens/pag_hechos_h.dart
+++ lib/screens/pag_hechos_h.dart
@@ -24,14 +24,18 @@ class _PagHechosHistoricosState extends State<PagHechosHistoricos> {
         location: '41.6191° N, 0.6232° E',
         image: 'assets/images_historical_events/Revolta d\'Indíbil i Mandoni.jpg',
         lat: 41.6191230, lng: 0.6232056, range: 1480.59, heading: -25.26, tilt: 65.45,
-        description: item.description,
+        epoca: 'Edat Antiga',
+        fechaInici: '206 aC', fechaFi: '205 aC',
+        description: 'Indíbil i Mandoni, líders ibers, es van rebel·lar contra Roma en comprendre que Escipió no portaria la independència, sinó una nova ocupació. L\'any 205 aC, van formar una gran coalició de pobles de la vall de l\'Ebre per expulsar els invasors. Tanmateix, es van enfrontar a la superioritat tàctica de les legions en una batalla decisiva. Indíbil va morir en combat lluitant heroicament, cosa que va desmuntar la resistència de les seves tropes.',
       ),
       POI(
         name: 'Batalla de Ilerda 49 aC',
         location: '41.6191° N, 0.6232° E',
         image: 'assets/images_historical_events/Batalla de Ilerda 49 aC.png',
         lat: 41.6191230, lng: 0.6232056, range: 1480.59, heading: -25.26, tilt: 65.45,
-        description: item.description,
+        epoca: 'Edat Antiga',
+        fechaInici: '49 aC', fechaFi: '49 aC',
+        description: 'La Batalla d\'Ilerda va ser un enfrontament magistral de la Segona Guerra Civil romana en què Juli Cèsar va derrotar els generals de Pompeu, Afrani i Petreu, als voltants de l\'actual Lleida. Malgrat quedar aïllat i sense subministraments per una gran crescuda del riu Segre, Cèsar va capgirar la situació amb una brillant maniobra d\'enginyeria i una estratègia de desgast que va envoltar les legions pompeianes.',
       ),
     ],
     'Edad Media Temprana': [
@@ -40,21 +44,27 @@ class _PagHechosHistoricosState extends State<PagHechosHistoricos> {
         location: '41.6180° N, 0.6258° E',
         image: 'assets/images_historical_events/invasio_musulmana.jpg',
         lat: 41.6180451, lng: 0.6258326, range: 361.12, heading: 12.41, tilt: 0.0,
-        description: item.description,
+        epoca: 'Edat Mitjana',
+        fechaInici: '716', fechaFi: '719',
+        description: 'La ciutat va ser ocupada per les tropes àrabs i berbers entre els anys 716 i 719, aprofitant la ràpida descomposició del regne visigot. Sota el nom de Lārida, es va transformar en una fortalesa estratègica de la Marca Superior. Durant aquest període, els musulmans van desenvolupar un avançat sistema de regadiu a l\'horta i van erigir la Suda, una impressionant alcassaba sobre la roca sobirana.',
       ),
       POI(
         name: 'Setge de Lleida (800)',
         location: '41.6191° N, 0.6232° E',
         image: 'assets/images_historical_events/Setge de lleida (800).png',
         lat: 41.6191230, lng: 0.6232056, range: 1480.59, heading: -25.26, tilt: 65.45,
-        description: item.description,
+        epoca: 'Edat Mitjana',
+        fechaInici: '800', fechaFi: '800',
+        description: 'L\'any 800, les tropes carolíngies de Lluís el Pietós van assetjar Lleida amb l\'objectiu d\'expandir la Marca Hispànica cap al sud dels Pirineus. L\'exèrcit franc va devastar els voltants de la ciutat per ofegar-la econòmicament. Davant l\'assetjament, el valí de Lleida va pactar una treva de tres anys i el pagament de tributs als francs.',
       ),
       POI(
         name: 'Setge de Lleida (884)',
         location: '41.6191° N, 0.6232° E',
         image: 'assets/images_historical_events/Setge de lleida (884).png',
         lat: 41.6191230, lng: 0.6232056, range: 1480.59, heading: -25.26, tilt: 65.45,
-        description: item.description,
+        epoca: 'Edat Mitjana',
+        fechaInici: '884', fechaFi: '884',
+        description: 'El comte Guifré el Pilós va atacar Lleida l\'any 884 com a resposta a la fortificació de la ciutat per part dels musulmans. La ciutat estava governada pel valí Ismaïl ibn Mussa, membre de la poderosa família dels Banu Qasi. L\'atac de Guifré va ser un fracàs militar: les cròniques àrabs parlen d\'una gran mortaldat entre les tropes catalanes.',
       ),
     ],
     'Reconquista / Alta Edad Media': [
@@ -63,14 +73,18 @@ class _PagHechosHistoricosState extends State<PagHechosHistoricos> {
         location: '41.6090° N, 0.6103° E',
         image: 'assets/images_historical_events/reconquista_cristiana_1149.jpg',
         lat: 41.6089691, lng: 0.6103237, range: 1159.27, heading: -53.72, tilt: 56.84,
-        description: item.description,
+        epoca: 'Edat Mitjana',
+        fechaInici: '1149', fechaFi: '1149',
+        description: 'Les hostes de Ramon Berenguer IV i Ermengol VI d\'Urgell van establir el seu campament estratègic al turó de Gardeny durant la primavera de 1149 per iniciar el setge definitiu de la ciutat. La resistència musulmana finalment es va trencar el 24 d\'octubre de 1149, quan la guarnició almoràvit va capitular. Aquesta victòria va provocar la caiguda immediata de Fraga i Mequinensa.',
       ),
       POI(
         name: 'Unió del regne de Aragó i comtat de Barcelona',
         location: '41.6168° N, 0.6255° E',
         image: 'assets/images_historical_events/Unio del regne de Arago i comtat de barcelona.jpg',
         lat: 41.6167910, lng: 0.6254991, range: 1480.59, heading: -25.26, tilt: 65.45,
-        description: item.description,
+        epoca: 'Edat Mitjana',
+        fechaInici: '1150', fechaFi: '1150',
+        description: 'A l\'agost de 1150, el Castell de la Suda de Lleida va acollir el casament entre Peronella d\'Aragó i Ramon Berenguer IV. Aquest enllaç va ser l\'acte fundacional de la Corona d\'Aragó, unint el Regne d\'Aragó amb el Comtat de Barcelona i creant una potència política que respectava les lleis i els furs de cada territori.',
       ),
     ],
     'Edad Media / Baja Edad Media': [
@@ -79,21 +93,27 @@ class _PagHechosHistoricosState extends State<PagHechosHistoricos> {
         location: '41.6180° N, 0.6258° E',
         image: 'assets/images_historical_events/jura_fidelitat_jaume1.jpg',
         lat: 41.6180451, lng: 0.6258326, range: 361.12, heading: 12.41, tilt: 0.0,
-        description: item.description,
+        epoca: 'Edat Mitjana',
+        fechaInici: '1214', fechaFi: '1214',
+        description: 'Les Corts de Lleida de 1214 es consideren les primeres de la història catalana amb participació dels tres estaments, convocades amb urgència per jurar fidelitat al nen Jaume I i estabilitzar la Corona d\'Aragó després de la catastròfica mort de Pere el Catòlic a la batalla de Muret. L\'assemblea va designar el comte Sanç de Rosselló com a procurador general per governar el territori.',
       ),
       POI(
         name: 'Primera universitat del regne d\'Aragó',
         location: '41.6147° N, 0.6199° E',
         image: 'assets/images_historical_events/Primera universitat del regne d\'arago.jpg',
         lat: 41.6146803, lng: 0.6198760, range: 270.14, heading: -70.55, tilt: 47.35,
-        description: item.description,
+        epoca: 'Edat Mitjana',
+        fechaInici: '1300', fechaFi: '1300',
+        description: 'Fundat l\'any 1300 per Jaume II després d\'una butlla papal de 1297, l\'Estudi General de Lleida va ser la primera universitat de Catalunya i de la Corona d\'Aragó. Durant segles va destacar com a centre de referència en Dret, Medicina i Filosofia. L\'any 1717, Felip V va clausurar la institució. La Universitat de Lleida actual es va refundar el 1991.',
       ),
       POI(
         name: 'Setge de Lleida (1413)',
         location: '41.6191° N, 0.6232° E',
         image: 'assets/images_historical_events/Setge de lleida (1413).jpg',
         lat: 41.6191230, lng: 0.6232056, range: 1480.59, heading: -25.26, tilt: 65.45,
-        description: item.description,
+        epoca: 'Edat Mitjana',
+        fechaInici: '1413', fechaFi: '1414',
+        description: 'Després del Compromís de Casp, Jaume II d\'Urgell es va alçar contra el nou rei Ferran I. El juny de 1413, les tropes del comte van intentar prendre Lleida per sorpresa. La ciutat es va mantenir fidel a Ferran I i les seves defenses van repel·lir l\'atac inicial, forçant el comte d\'Urgell a retirar-se cap a Balaguer.',
       ),
     ],
     'Edad Moderna': [
@@ -102,35 +122,45 @@ class _PagHechosHistoricosState extends State<PagHechosHistoricos> {
         location: '41.6100° N, 0.6367° E',
         image: 'assets/images_historical_events/La batalla de Lleida (1642).jpg',
         lat: 41.6100091, lng: 0.6367412, range: 2017.02, heading: -53.71, tilt: 56.85,
-        description: item.description,
+        epoca: 'Edat Moderna',
+        fechaInici: '1642', fechaFi: '1642',
+        description: 'El 7 d\'octubre de 1642, en plena Guerra dels Segadors, l\'exèrcit espanyol va intentar recuperar Lleida. Les tropes de la Monarquia Hispànica, dirigides pel marquès de Leganés, van topar amb l\'exèrcit franco-català del mariscal La Mothe-Houdancourt. Malgrat la seva superioritat numèrica, les forces espanyoles van patir una derrota estrepitosa.',
       ),
       POI(
         name: 'Setge de Lleida (1644)',
         location: '41.6149° N, 0.6204° E',
         image: 'assets/images_historical_events/Setge de lleida (1644).png',
         lat: 41.6149206, lng: 0.6204228, range: 1229.41, heading: -52.03, tilt: 60.58,
-        description: item.description,
+        epoca: 'Edat Moderna',
+        fechaInici: '1644', fechaFi: '1644',
+        description: 'El maig de 1644, l\'exèrcit de Felip IV, liderat per Felip de Silva, va posar setge a la ciutat de Lleida. Les tropes de la Monarquia Hispànica van aconseguir derrotar l\'exèrcit francès de La Mothe-Houdancourt. La capitulació final, a finals de juliol de 1644, va representar una de les victòries militars més importants per a Espanya durant la Guerra dels Segadors.',
       ),
       POI(
         name: 'Setge de Lleida (1646)',
         location: '41.6191° N, 0.6232° E',
         image: 'assets/images_historical_events/Setge de lleida (1646).jpg',
         lat: 41.6191230, lng: 0.6232056, range: 1480.59, heading: -25.26, tilt: 65.45,
-        description: item.description,
+        epoca: 'Edat Moderna',
+        fechaInici: '1646', fechaFi: '1646',
+        description: 'El maig de 1646, les tropes franceses sota el comandament del comte d\'Harcourt van assetjar Lleida. L\'exèrcit espanyol, dirigit pel marquès de Leganés, va aconseguir trencar el setge mitjançant un audaciós atac sorpresa nocturn. La derrota francesa va ser tan contundent que Harcourt es va veure obligat a fugir abandonant artilleria i subministraments.',
       ),
       POI(
         name: 'Setge de Lleida (1647)',
         location: '41.6191° N, 0.6232° E',
         image: 'assets/images_historical_events/Setge de lleida (1647).png',
         lat: 41.6191230, lng: 0.6232056, range: 1480.59, heading: -25.26, tilt: 65.45,
-        description: item.description,
+        epoca: 'Edat Moderna',
+        fechaInici: '1647', fechaFi: '1647',
+        description: 'El maig de 1647, el príncep de Condé, heroi francès de Rocroi, va iniciar un nou setge sobre Lleida. La defensa va recaure en el governador Gregorio Brito, que va resistir amb tenacitat els assalts. Davant la impossibilitat de trencar les muralles, Condé es va veure obligat a aixecar el setge al juny, patint una de les poques derrotes de la seva carrera militar.',
       ),
       POI(
         name: 'Setge de Lleida (1707)',
         location: '41.6191° N, 0.6232° E',
         image: 'assets/images_historical_events/Setge de lleida (1707)png.png',
         lat: 41.6191230, lng: 0.6232056, range: 1480.59, heading: -25.26, tilt: 65.45,
-        description: item.description,
+        epoca: 'Edat Moderna',
+        fechaInici: '1707', fechaFi: '1707',
+        description: 'El setembre de 1707, les tropes borbòniques del duc d\'Orleans van assetjar Lleida. Després d\'un mes d\'intensos bombardejos, els borbònics van assaltar la ciutat el 12 d\'octubre. La victòria borbònica va suposar un càstig sever: la ciutat va ser saquejada, va perdre els seus furs i la seva universitat fou clausurada.',
       ),
     ],
     'Edad Contemporánea': [
@@ -139,14 +169,18 @@ class _PagHechosHistoricosState extends State<PagHechosHistoricos> {
         location: '41.6191° N, 0.6232° E',
         image: 'assets/images_historical_events/Setge de lleida (1810).png',
         lat: 41.6191230, lng: 0.6232056, range: 1480.59, heading: -25.26, tilt: 65.45,
-        description: item.description,
+        epoca: 'Edat Contemporània',
+        fechaInici: '1810', fechaFi: '1810',
+        description: 'El maig de 1810, durant la Guerra del Francès, les tropes napoleòniques del mariscal Suchet van assetjar Lleida. La guarnició espanyola, liderada pel general Jaime García-Conde, va resistir un violent bombardeig fins que els francesos van aconseguir obrir bretxes a les muralles. La capitulació el 14 de maig va permetre als francesos controlar una plaça estratègica clau.',
       ),
       POI(
         name: 'Batalla de Lleida (1938)',
         location: '41.6191° N, 0.6232° E',
         image: 'assets/images_historical_events/Batalla de Lleida (1938).jpeg',
         lat: 41.6191230, lng: 0.6232056, range: 1480.59, heading: -25.26, tilt: 65.45,
-        description: item.description,
+        epoca: 'Edat Contemporània',
+        fechaInici: '1938', fechaFi: '1938',
+        description: 'La Batalla de Lleida, culminada el 3 d\'abril de 1938, va marcar l\'entrada definitiva de les tropes de Franco a Catalunya després de trencar el front d\'Aragó. El general Yagüe va liderar l\'assalt contra una defensa republicana que va resistir ferotgement al nucli urbà sota intensos bombardejos. La caiguda de Lleida va permetre a Franco derogar l\'Estatut d\'Autonomia de Catalunya.',
       ),
     ],
   };
@@ -330,36 +364,36 @@ class _PagHechosHistoricosState extends State<PagHechosHistoricos> {
               Expanded(
                 child: pois.isEmpty
                     ? Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.search_off_rounded,
-                              size: 80,
-                              color: Colors.grey[300],
-                            ),
-                            const SizedBox(height: 16),
-                            Text(
-                              T.s('no_results_found'),
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.grey[600],
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                    : ListView.builder(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 0,
-                        ),
-                        physics: const BouncingScrollPhysics(),
-                        itemCount: pois.length,
-                        itemBuilder: (context, index) =>
-                            _cardPunto(pois[index]),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.search_off_rounded,
+                        size: 80,
+                        color: Colors.grey[300],
                       ),
+                      const SizedBox(height: 16),
+                      Text(
+                        T.s('no_results_found'),
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey[600],
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+                    : ListView.builder(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 0,
+                  ),
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: pois.length,
+                  itemBuilder: (context, index) =>
+                      _cardPunto(pois[index]),
+                ),
               ),
             ],
           ),
@@ -447,19 +481,19 @@ class _PagHechosHistoricosState extends State<PagHechosHistoricos> {
         return categories
             .map(
               (cat) => PopupMenuItem<String>(
-                value: cat,
-                child: Text(
-                  cat == 'All' ? T.s('show_all') : cat,
-                  style: TextStyle(
-                    fontSize: 13,
-                    fontWeight: _selectedCategory == cat
-                        ? FontWeight.w800
-                        : FontWeight.w500,
-                    color: const Color(0xFF1C1C1E),
-                  ),
-                ),
+            value: cat,
+            child: Text(
+              cat == 'All' ? T.s('show_all') : cat,
+              style: TextStyle(
+                fontSize: 13,
+                fontWeight: _selectedCategory == cat
+                    ? FontWeight.w800
+                    : FontWeight.w500,
+                color: const Color(0xFF1C1C1E),
               ),
-            )
+            ),
+          ),
+        )
             .toList();
       },
       child: Container(
--- lib/services/lg_service.dart
+++ lib/services/lg_service.dart
@@ -221,41 +221,50 @@ fi
       debugPrint('LGService: Error al resetear refresco: $e');
     }
   }
-  Future<void> sendBalloon(POI poi, String description) async {
+  Future<void> sendBalloon(POI poi) async {
     final int slaveNo = _conn.screens == 5 ? 4 : 2;
+
+    final String epocaLine = (poi.epoca != null && poi.epoca!.isNotEmpty)
+        ? '<p style="font-size:12px;color:#A0856A;margin:0 0 12px 0;text-transform:uppercase;letter-spacing:1px;">\${poi.epoca}</p>'
+        : '';
+
+    final String fechaLine = (poi.fechaInici != null && poi.fechaInici!.isNotEmpty)
+        ? '<p style="font-size:12px;color:#A0856A;margin:0 0 16px 0;">\${poi.fechaInici}\${poi.fechaFi != null && poi.fechaFi != poi.fechaInici ? " – \${poi.fechaFi}" : ""}</p>'
+        : '';
+
+    final String descLine = (poi.description != null && poi.description!.isNotEmpty)
+        ? '<p style="font-size:14px;line-height:1.75;color:#E0D8CC;margin:0;">\${poi.description}</p>'
+        : '';
 
     final String kml = '''<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
   <Document>
     <Placemark>
-      <name>${poi.name}</name>
+      <name>\${poi.name}</name>
       <description><![CDATA[
         <html>
-        <body style="margin:0;padding:0;background:#1C1C1E;font-family:Georgia,serif;color:#F5F1E9;">
-          <div style="padding:24px;">
-            <h2 style="font-size:22px;font-weight:400;margin:0 0 8px 0;color:#F5F1E9;">
-              ${poi.name}
+        <body style="margin:0;padding:0;background:#1C1C1E;font-family:Georgia,serif;color:#F5F1E9;width:100%;height:100%;">
+          <div style="padding:28px 28px 24px 28px;">
+            <h2 style="font-size:24px;font-weight:400;margin:0 0 10px 0;color:#F5F1E9;line-height:1.3;">
+              \${poi.name}
             </h2>
-            <div style="width:40px;height:2px;background:#C8A96E;margin-bottom:16px;"></div>
-            <p style="font-size:13px;color:#C8A96E;margin:0 0 16px 0;">
-              ${poi.location}
-            </p>
-            <p style="font-size:14px;line-height:1.7;color:#E0D8CC;margin:0;">
-              $description
-            </p>
+            <div style="width:40px;height:2px;background:#C8A96E;margin-bottom:14px;"></div>
+            \$epocaLine
+            \$fechaLine
+            \$descLine
           </div>
         </body>
         </html>
       ]]></description>
       <Point>
-        <coordinates>${poi.lng ?? 0.6268},${poi.lat ?? 41.6147},0</coordinates>
+        <coordinates>\${poi.lng ?? 0.6268},\${poi.lat ?? 41.6147},0</coordinates>
       </Point>
     </Placemark>
   </Document>
 </kml>''';
 
     await _conn.execute(
-        "cat <<'KMLEOF' > /var/www/html/kml/slave_$slaveNo.kml\n$kml\nKMLEOF");
+        "cat <<'KMLEOF' > /var/www/html/kml/slave_\$slaveNo.kml\n\$kml\nKMLEOF");
   }
 
   Future<void> clearBalloon() async {
